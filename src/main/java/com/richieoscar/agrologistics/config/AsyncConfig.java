246d55d84ed3891c66c842d4c7b3b78cf889e3d7
Oscar Anyiam <anyiam@systemspecs.com.ng>
2023-03-22 23:27:45 +0100

    updated ASYNC CONFIG

--- src/main/java/com/richieoscar/agrologistics/config/AsyncConfig.java
+++ src/main/java/com/richieoscar/agrologistics/config/AsyncConfig.java
@@ -1,12 +1,14 @@
 package com.richieoscar.agrologistics.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @EnableAsync
+@Configuration
 public class AsyncConfig {
     @Bean
     public Executor threadExecutor(){
